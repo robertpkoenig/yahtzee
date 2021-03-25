22d83c1c0d75f2d9d8472e7e2262d8e3aa288466
Robert Koenig <robertpkoenig@gmail.com>
2021-03-25 13:29:43 +0000

    make test intentionally fail

--- src/test/java/stacs/yahtzee/DieUnitTests.java
+++ src/test/java/stacs/yahtzee/DieUnitTests.java
@@ -13,7 +13,8 @@ public class DieUnitTests {
     public void testDieInitialNegativeOne() {
         IDie die = new Die();
         // intentionally making this test fail for the purposes of testing the CI?CD workflow on github
-        assertEquals(-1, die.getCurrentFace());
+        // it should equal -1, but here I've changed it to 1000
+        assertEquals(1000, die.getCurrentFace());
     } 
 
     @Test
